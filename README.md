6fc23a036ade6d8ad31d6b035d01fd92125ca572
SammanTasnim <101858287+SammanTasnim@users.noreply.github.com>
2025-07-03 00:28:29 +0600

    final changes

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# QA Testing Quiz
+# QA Code Quiz – Test Suite
 
-### Scenario
-The frontend team has developed a prototype login portal for an up and coming platform.
-However, they have not implemented any testing yet and it is up to you to do so.
+This repository contains automated using cypress and manual test case.
 
-As the QA developer, what is tested and how it is tested is up to you.
-Management simply asks that these tests provide as much evidence as possible of the platform's reliability.
+---
 
-### Notes
-- Submission must include a link to a public fork/clone of this repository
-- We typically use Jest for testing node.js/API related logic and Cypress for testing UI functionality, however, you are more than welcome to use any testing framework you desire so long as you are able to provide reasonable justification
+[Manual Test Cases](https://docs.google.com/spreadsheets/d/1SXUzMHMtkn94PoTAhxfwWh5WNR8Ygd6uUirZle4lgT8/edit?usp=sharing)
+---
+
+
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,23 +5,23 @@ describe("Test Login Page", () => {
     });
   
     it("Check render the fields", () => {
-        //Header text check
-        cy.get('.sc-bdVaJa > div').should("contain.text", "qa.code-quiz.dev");
+      //Header text check
+      cy.get('.sc-bdVaJa > div').should("contain.text", "qa.code-quiz.dev");
 
-        //Username input field check
-        cy.get('[placeholder="Enter Username"]').should("exist");
+      //Username input field check
+      cy.get('[placeholder="Enter Username"]').should("exist");
 
-        //Pasword input field check
-        cy.get('[placeholder="password"]').should("exist");
+      //Pasword input field check
+      cy.get('[placeholder="password"]').should("exist");
 
-        //Login button check
-        cy.get('.sc-bZQynM').should("exist");
+      //Login button check
+      cy.get('.sc-bZQynM').should("exist");
 
-        //Login button text check
-        cy.get('.sc-bZQynM').should("contain.text", "LOGIN");   
+      //Login button text check
+      cy.get('.sc-bZQynM').should("contain.text", "LOGIN");   
 
-        //Forget password text check
-        cy.get('.sc-ifAKCX > div').should("contain.text", "If you do not have an account, contact an admin");
+      //Forget password text check
+      cy.get('.sc-ifAKCX > div').should("contain.text", "If you do not have an account, contact an admin");
 
     });
 
@@ -29,57 +29,82 @@ describe("Test Login Page", () => {
       cy.contains('button', 'LOGIN').focus().type('{enter}');
     });
 
-    it("Check validations", () => {
+    it("Check login with valid credentials", () => {
+      //Enter valid username
+      cy.get('[placeholder="Enter Username"]').type("SomeName");
+      //Enter valid password
+      cy.get('[placeholder="password"]').type("TopSecret1234!");
+      //Click on login button
+      cy.get(".sc-bZQynM.cGmBje").click();
+
+      cy.wait(1000); // Wait loading the page
+      cy.contains('Hello SomeName').should('exist');
+      cy.contains('Favourite Fruit').should('exist');
+      cy.contains('Favourite Movie').should('exist');
+      cy.contains('Favourite Number').should('exist');
+      cy.contains('button', 'LOGOUT').click();
+    });
+
+    it("Check validation with invalid credentials", () => {
+      //Enter valid username
+      cy.get('[placeholder="Enter Username"]').type("InvalidUser");
+      //Enter valid password
+      cy.get('[placeholder="password"]').type("InvalidPassword123");
+      //Click on login button
+      cy.get(".sc-bZQynM.cGmBje").click();
+
+      cy.wait(1000); // Wait loading the page
+      cy.contains('Invalid credentials').should('exist');
+    });
+
+    it("Check validations for empty fields", () => {
       //Click on login button without entering data
       cy.contains('button', 'LOGIN').click();
       
       //Username empty
       cy.contains('Username is required.').should('be.visible');
+      //Password empty
       cy.contains('Password is required.').should('be.visible');
-    }); 
+    });
 
     it("Responsiveness 1280*800", () => {
       cy.viewport(1280, 800);
       cy.get('.sc-bdVaJa > div').should("contain.text", "qa.code-quiz.dev");
-        //Username input field check
-        cy.get('[placeholder="Enter Username"]').should("exist");
 
-        //Pasword input field check
-        cy.get('[placeholder="password"]').should("exist");
+      //Username input field check
+      cy.get('[placeholder="Enter Username"]').should("exist");
 
-        //Login button check
-        cy.get('.sc-bZQynM').should("exist");
+      //Pasword input field check
+      cy.get('[placeholder="password"]').should("exist");
 
-        //Login button text check
-        cy.get('.sc-bZQynM').should("contain.text", "LOGIN");   
+      //Login button check
+      cy.get('.sc-bZQynM').should("exist");
 
-        //Forget password text check
-        cy.get('.sc-ifAKCX > div').should("exist");
+      //Login button text check
+      cy.get('.sc-bZQynM').should("contain.text", "LOGIN");   
 
-        cy.get('.sc-ifAKCX > div').should("contain.text", "If you do not have an account, contact an admin");
-
+      //Forget password text check
+      cy.get('.sc-ifAKCX > div').should("contain.text", "If you do not have an account, contact an admin");
 
     });
     it("Responsiveness 412*995", () => {
       cy.viewport(412, 995);
       cy.get('.sc-bdVaJa > div').should("contain.text", "qa.code-quiz.dev");
-        //Username input field check
-        cy.get('[placeholder="Enter Username"]').should("exist");
 
-        //Pasword input field check
-        cy.get('[placeholder="password"]').should("exist");
+      //Username input field check
+      cy.get('[placeholder="Enter Username"]').should("exist");
 
-        //Login button check
-        cy.get('.sc-bZQynM').should("exist");
+      //Pasword input field check
+      cy.get('[placeholder="password"]').should("exist");
 
-        //Login button text check
-        cy.get('.sc-bZQynM').should("contain.text", "LOGIN");   
+      //Login button check
+      cy.get('.sc-bZQynM').should("exist");
 
-        //Forget password text check
-        cy.get('.sc-ifAKCX > div').should("exist");
+      //Login button text check
+      cy.get('.sc-bZQynM').should("contain.text", "LOGIN");   
 
-        cy.get('.sc-ifAKCX > div').should("contain.text", "If you do not have an account, contact an admin");
-
+      //Forget password text check
+      cy.get('.sc-ifAKCX > div').should("contain.text", "If you do not have an account, contact an admin");
 
     });
     it("Responsiveness 1024*600", () => {
